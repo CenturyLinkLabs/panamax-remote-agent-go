ee102a9a8e0916541683bb2052235636aa998ba5
Alex <me@alexwelch.com>
2015-03-27 09:23:33 -0600

    Use ENV vars where appropriate

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main // import "github.com/CenturyLinkLabs/panamax-remote-agent-go"
 
 import (
-	// "os"
 	"log"
+	"os"
 	"regexp"
 
 	"github.com/CenturyLinkLabs/panamax-remote-agent-go/adapter"
@@ -12,19 +12,37 @@ import (
 )
 
 func main() {
-	p, err := repo.MakePersister("db/agent.db")
+	p, err := repo.MakePersister(dbLocation())
 	if err != nil {
 		log.Fatal(err)
 	}
 	c := adapter.MakeClient(adapterEndpoint())
 	dm := agent.MakeDeploymentManager(p, c)
 	s := api.MakeServer(dm)
-	s.Start(":1234")
+	s.Start(serverPort())
+}
+
+func dbLocation() string {
+	l := os.Getenv("DB_LOCATION")
+	if l == "" {
+		l = "db/agent.db"
+	}
+	return l
+}
+
+func serverPort() string {
+	p := os.Getenv("SERVER_PORT")
+	if p == "" {
+		p = "3000"
+	}
+	return ":" + p
 }
 
 func adapterEndpoint() string {
-	// url := os.Getenv("ADAPTER_PORT")
-	url := "tcp://localhost:1234"
+	url := os.Getenv("ADAPTER_PORT")
+	if url == "" {
+		url = "tcp://localhost:3001"
+	}
 	r, _ := regexp.Compile("^tcp")
 	return r.ReplaceAllString(url, "http")
 }
